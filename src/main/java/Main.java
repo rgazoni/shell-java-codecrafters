9d0b5056f81dabbe13b875aabe6af641694e0e5f
Ramon Gazoni <rgazoni@Ramons-MacBook-Air.local>
2025-02-05 14:51:37 -0300

    exit scape hatch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,8 +6,12 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.print("$ ");
-
             String input = scanner.nextLine();
+
+            if (input.equals("exit 0")) {
+                break;
+            }
+
             System.out.println(input + ": command not found");
         }
 
